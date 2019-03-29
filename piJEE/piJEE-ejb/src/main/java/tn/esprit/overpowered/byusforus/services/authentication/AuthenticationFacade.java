f5f06b961880e9b3ff38dee968b6597db23bc68f
aminos <amri.amine@protonmail.com>
2019-03-29 03:19:07 +0100

    made the service stateful

--- piJEE/piJEE-ejb/src/main/java/tn/esprit/overpowered/byusforus/services/authentication/AuthenticationFacade.java
+++ piJEE/piJEE-ejb/src/main/java/tn/esprit/overpowered/byusforus/services/authentication/AuthenticationFacade.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.Stateful;
 import javax.ejb.Stateless;
 import javax.mail.MessagingException;
 import javax.persistence.EntityManager;
@@ -29,7 +30,7 @@ import tn.esprit.overpowered.byusforus.util.MailSender;
  *
  * @author EliteBook
  */
-@Stateless
+@Stateful
 public class AuthenticationFacade implements AuthenticationFacadeRemote {
 
     @PersistenceContext(unitName = "piJEE-ejb")
