f3a7ce32aaaa9d2f0233c2588a79fb096b626fd0
Amine Skhiri <mohamedamine.skhiri@esprit.tn>
2019-03-31 00:27:59 +0100

    Create claims entities

---
+++ piJEE/piJEE-ejb/src/main/java/tn/esprit/overpowered/byusforus/entities/reclamation/TypeReclamation.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.overpowered.byusforus.entities.reclamation;
+
+/**
+ *
+ * @author amine
+ */
+public enum TypeReclamation {Financier,Oeuvre,Systeme,Suggestion,Autre}
+
