0613e11e5d58f6adcd739ffab071115413064ea9
YessineAmor <yassine.amor@esprit.tn>
2019-04-14 14:32:59 +0100

    Created listing quizzes functionality

---
+++ piJEE/piJEE-web/src/main/java/tn/esprit/overpowered/byusforus/managedbeans/quiz/sessionbeans/QuizController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.overpowered.byusforus.managedbeans.quiz.sessionbeans;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import tn.esprit.overpowered.byusforus.entities.quiz.Quiz;
+import tn.esprit.overpowered.byusforus.services.quiz.QuizFacadeLocal;
+
+/**
+ *
+ * @author Yassine
+ */
+@ManagedBean
+@SessionScoped
+public class QuizController implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EJB
+    private QuizFacadeLocal quizFacade;
+
+    /**
+     * Creates a new instance of QuizController
+     */
+    public QuizController() {
+    }
+
+    public QuizFacadeLocal getQuizFacade() {
+        return quizFacade;
+    }
+
+    public void setQuizFacade(QuizFacadeLocal quizFacade) {
+        this.quizFacade = quizFacade;
+    }
+
+    public List<Quiz> getAllQuizzes() {
+        return quizFacade.findAll();
+    }
+
+    public String getFirstQuizName() {
+        return "sdfds";
+    }
+
+}
