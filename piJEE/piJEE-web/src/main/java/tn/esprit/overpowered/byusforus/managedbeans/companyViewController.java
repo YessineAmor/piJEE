72d13ecba52fda1d382d0c2c8ad55a328f8aed79
MotazSouid <moatez.souid@esprit.tn>
2019-04-19 13:59:31 +0200

    added company list

---
+++ piJEE/piJEE-web/src/main/java/tn/esprit/overpowered/byusforus/managedbeans/companyViewController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tn.esprit.overpowered.byusforus.managedbeans;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import tn.esprit.overpowered.byusforus.entities.users.CompanyProfile;
+import tn.esprit.overpowered.byusforus.services.users.CompanyProfileFacadeLocal;
+
+/**
+ *
+ * @author EliteBook
+ */
+@ManagedBean
+@SessionScoped
+public class companyViewController implements Serializable {
+    private CompanyProfile selectedCompany;
+    
+    @EJB
+    private CompanyProfileFacadeLocal compFacade;
+    
+    public List<CompanyProfile> listComp()
+    {
+        return compFacade.findAll();
+    }
+
+    public CompanyProfile getSelectedCompanies() {
+        return selectedCompany;
+    }
+
+    public void setSelectedCompanies(CompanyProfile selectedCompanies) {
+        this.selectedCompany = selectedCompanies;
+    }
+
+    public CompanyProfileFacadeLocal getCompFacade() {
+        return compFacade;
+    }
+
+    public void setCompFacade(CompanyProfileFacadeLocal compFacade) {
+        this.compFacade = compFacade;
+    }
+    
+}
