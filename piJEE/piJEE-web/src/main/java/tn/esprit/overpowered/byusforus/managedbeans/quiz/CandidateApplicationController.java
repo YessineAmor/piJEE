b566f2323fbffde70d960a6de39f0cab6171dced
YessineAmor <yassine.amor@esprit.tn>
2019-04-26 19:27:23 +0100

    Added job application

--- piJEE/piJEE-web/src/main/java/tn/esprit/overpowered/byusforus/managedbeans/quiz/CandidateApplicationController.java
+++ piJEE/piJEE-web/src/main/java/tn/esprit/overpowered/byusforus/managedbeans/quiz/CandidateApplicationController.java
@@ -1,11 +1,16 @@
 package tn.esprit.overpowered.byusforus.managedbeans.quiz;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import tn.esprit.overpowered.byusforus.entities.candidat.CandidateApplication;
 import tn.esprit.overpowered.byusforus.managedbeans.util.JsfUtil;
 import tn.esprit.overpowered.byusforus.managedbeans.util.JsfUtil.PersistAction;
 
 import java.io.Serializable;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,7 +22,12 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
+import javax.servlet.http.Part;
+import tn.esprit.overpowered.byusforus.entities.entrepriseprofile.JobOffer;
+import tn.esprit.overpowered.byusforus.entities.users.Candidate;
 import tn.esprit.overpowered.byusforus.services.candidat.CandidateApplicationFacadeLocal;
+import tn.esprit.overpowered.byusforus.util.JobApplicationState;
+import util.authentication.Authenticator;
 
 @ManagedBean
 @SessionScoped
@@ -27,8 +37,17 @@ public class CandidateApplicationController implements Serializable {
     private CandidateApplicationFacadeLocal ejbFacade;
     private List<CandidateApplication> items = null;
     private CandidateApplication selected;
+    private Part file;
 
     public CandidateApplicationController() {
+    }
+
+    public Part getFile() {
+        return file;
+    }
+
+    public void setFile(Part file) {
+        this.file = file;
     }
 
     public CandidateApplication getSelected() {
@@ -53,6 +72,47 @@ public class CandidateApplicationController implements Serializable {
         selected = new CandidateApplication();
         initializeEmbeddableKey();
         return selected;
+    }
+
+    public String showApplicationForm(JobOffer jobOffer) {
+        selected = new CandidateApplication();
+        selected.setJobOffer(jobOffer);
+        return "/views/front/JobApplication/apply_to_job_form?faces-redirect=true";
+    }
+
+    public void saveResumeFile() throws IOException {
+        String randomFileName = new Random().nextInt() + file.getSubmittedFileName();
+        this.selected.setResume(randomFileName);
+        try (InputStream input = file.getInputStream()) {
+            Files.copy(input, new File("../standalone/deployments/piJEE-web-1.0.war/", randomFileName).toPath());
+        }
+    }
+
+    public String submitApplication() {
+        this.selected.setCandidate((Candidate) Authenticator.currentSession.getUser());
+        this.selected.setAdditionalInfo("Waiting for company decision");
+        this.create();
+        return "/views/candidate/candidatesView?faces-redirect=true";
+    }
+
+    public String inviteToTakeQuiz() {
+        this.selected.setJobApplicationState(JobApplicationState.INVITED_FOR_QUIZ);
+        this.selected.setAdditionalInfo("Waiting for candidate to take quiz");
+        this.update();
+        return "List?faces-redirect=true";
+    }
+
+    public String rejectCandidacy() {
+        this.selected.setJobApplicationState(JobApplicationState.REFUSED);
+        this.selected.setAdditionalInfo("candidacy refuesd.");
+        this.update();
+        return "List?faces-redirect=true";
+    }
+
+    public Boolean hasAlreadyApplied(JobOffer jobOffer) {
+        CandidateApplication cApp = ejbFacade.getApplicationByCandidateId(Authenticator.currentSession.getUser().getId(), jobOffer.getId());
+        this.selected = cApp;
+        return cApp != null;
     }
 
     public void create() {
